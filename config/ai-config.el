749e58d86c3ac08994869c5eae4cd979ceb6913b
Dougie Beney <git@dougie.io>
2026-07-15 00:03:43 -0400

    start of gptel

--- config/ai-config.el
+++ config/ai-config.el
@@ -1,3 +1,17 @@
 (provide 'ai-config)
 
 (use-package agent-shell)
+
+(use-package gptel
+    :config
+  ;; Configure the below securely
+  ;; (setq
+  ;;  gptel-model 'openai/example-model
+  ;;  gptel-backend (gptel-make-openai "My OpenAI compatible API"
+  ;;                  :host "host.example-llm.com"
+  ;;                  :endpoint "/v1/chat/completions"
+  ;;                  :stream t
+  ;;                  :protocol "https"
+  ;;                  :key "secret-key-here" ;can be a function that returns the key
+  ;;                  :models '(openai/example-model)))
+  )
